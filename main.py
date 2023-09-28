86d15d361c460652948d0a4624ac13fcb1f332c1
bertrandkuniedal <bertrandkuniedal@educ.college-montreal.qc.ca>
2023-09-28 11:36:02 -0400

    fini le template

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ while niveau_vie > 0:
     print("Que voulez-vous faire? \n1- Combattre cet adversaire\n2- Contourner cet adversaire et aller ouvrir une autre porte"
           "\n3- Afficher les regles du jeu\n4- Quitter le jeu")
     def choix():
-        # change la value globale de niveau_vie
+        # change la value des variable a global dans la fonction
         global niveau_vie
+        global nombre_defaite
+        global nombre_victoire
         x = str.strip(input("Votre choix: "))
         if x == "1":
             print("Vous avez ", niveau_vie, " vie et le monstre a ", force_adv, " de force, êtes vous certain? o/n ")
@@ -22,31 +24,43 @@ while niveau_vie > 0:
                 choix()
 
             win = False
-            while win != True:
+            while not win:
                 score_de = random.randint(1, 6)
                 print("Vous scorez un ", score_de, "!")
+
                 if score_de <= force_adv:
                     print("Vous perdez contre l'adversaire!")
                     print("Vous perdez", force_adv, "niveaux de vie")
                     niveau_vie = niveau_vie - force_adv
+                    nombre_defaite += 1
+                    print("Vous avez maintenant ", niveau_vie, " niveaux de vie!")
                 elif score_de > force_adv:
                     print("Vous gagnez contre l'adversaire!!!")
                     print("Vous recevez", force_adv, "niveaux de vie")
+                    niveau_vie = niveau_vie + force_adv
+                    nombre_victoire += 1
                     win = True
-            print("Vous avez gagné contre le monstre et ouvrez la porte devant vous avec ", niveau_vie, "niveaux de vie")
+            print("Vous avez gagné contre le monstre et ouvrez la porte devant vous avec ", niveau_vie,
+                      " niveaux de vie")
+
         elif x == "2":
             print("Vous contournez l'adversaire et ouvrez la porte devant vous")
 
 
         elif x == "3":
-            print("Les règles sont facile: \n Vous commencez avec 20 niveaux de vie (Vous en avez ", niveau_vie, "maintenant) "
-                "Vous allez vous battre contre un monstre d'une force entre 1 et 5. Pour l'attaquer vous allez lancer un dé"
-                 "de 1 - 6. Si vous scorez en dessous de son nombre de force, vous allez perdre le meme nombre de niveaux de vie"
-                "qu'il a de force" )
+            print("Les règles sont facile: \nVous commencez avec 20 niveaux de vie (Vous en avez ", niveau_vie, "maintenant) "
+                "Vous allez vous battre contre un monstre d'une force entre 1 et 5. \nPour l'attaquer vous allez lancer un dé"
+                 " de 1 - 6. \nSi vous scorez en dessous de son nombre de force, vous allez perdre le meme nombre de niveaux de vie"
+                " qu'il a de force. \nSi vous gagnez, vous allez recevoir le nombre de force de l'enemi en tant que niveaux de vie"
+                "\nLorsque vous recontrez un adversaire vous pouver choisir entre 4 options: \n1 pour le combattre, 2 pour l'éviter,"
+                " 3 pour revoir ces règles et 4 pour quitter le jeux" )
+            choix()
 
 
         elif x == "4":
-            print("")
+            print("Ok")
+            niveau_vie = 0
+
 
         else:
             print("Pas un choix valide, veuillez recommencer")
@@ -58,4 +72,4 @@ while niveau_vie > 0:
 
 
 
-print("Vous êtes mort... Vous avez eu")
+print("Vous êtes mort... Vous avez eu ", nombre_victoire, " victoires et ", nombre_defaite, " défaites")
